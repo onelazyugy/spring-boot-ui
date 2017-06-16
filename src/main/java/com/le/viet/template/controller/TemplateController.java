091d381349648507dadf6490271a89d7d4e86b08
viet <viet_t_le@homedepot.com>
2017-06-16 15:29:17 -0400

    POC thdsso

--- src/main/java/com/le/viet/template/controller/TemplateController.java
+++ src/main/java/com/le/viet/template/controller/TemplateController.java
@@ -23,9 +23,8 @@ public class TemplateController {
 
     @RequestMapping(value = "/thdLogin", method = RequestMethod.GET)
     public String thdLogin() throws Exception{
+        //login
         String thdLoginURL = "https://webapps-qa.homedepot.com/MYTHDPassport/rs/identity/thdLogin";
-
-
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
         MultiValueMap<String, String> map= new LinkedMultiValueMap<String, String>();
@@ -37,19 +36,17 @@ public class TemplateController {
 
         HttpEntity<MultiValueMap<String, String>> req = new HttpEntity<MultiValueMap<String, String>>(map, headers);
         ResponseEntity<String> res = restTemplate.postForEntity( thdLoginURL, req , String.class );
-
         System.out.println("RESPONSE: " + res.getBody());
 
+        //extract thdsso cookie
         HttpHeaders headers1 = res.getHeaders();
         List<String> list = headers1.get("Set-Cookie");
         System.out.println("list tostring: " + list.toString());
         String thdssoCookie = list.get(2);
-
         String cookie = thdssoCookie.replace("THDSSO=", "");
         int indexAtSemicolon = cookie.indexOf(";");
         cookie = cookie.substring(0, indexAtSemicolon);
         System.out.println("actual cookie: " + cookie);
-
         for(String cookies : list){
             System.out.println("cookies: " + cookies);
         }
@@ -60,12 +57,11 @@ public class TemplateController {
         System.out.println("responseEntity: " + responseEntity.getBody());
 
 
-        //is session valid
+        //is session valid, call when user go to different route to ensure user still has a session
         String isSessionValidURL = "https://webapps-qa.homedepot.com/MYTHDPassport/rs/identity/isSessionValid?callingProgram=ps-giftcard-security&thdsso=" + cookie;
         ResponseEntity<String> sessionResponse = restTemplate.getForEntity(isSessionValidURL, String.class);
         System.out.println("sessionResponse: " + sessionResponse);
 
-
-        return res.getBody();
+        return "success!";
     }
 }
